e7fac925a62d667bad746f5372af45b6d5885a5e
Sonu Kumar <sksonu8544@gmail.com>
2024-05-18 12:36:27 +0530

    Fix post data

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,10 @@
 import GetData1 from "@/components/GetData1";
+import Postdata1 from "@/components/Postdata1";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-10">
+      <Postdata1 />
       <GetData1 />
     </main>
   );
