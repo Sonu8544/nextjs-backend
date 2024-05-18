2aef80f9020dc0495b58dfc78f47fa6c8ee48e68
Sonu Kumar <sksonu8544@gmail.com>
2024-05-18 10:57:53 +0530

    dev post data form client to server using api

---
+++ src/app/form/page.js
@@ -0,0 +1,10 @@
+import Postdata1 from '@/components/Postdata1'
+import React from 'react'
+
+export default function page() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-10">
+            <Postdata1 />
+        </main>
+    )
+}
