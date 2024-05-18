934a3d76d38e85b9d03c5d8c41b117c120de7669
Sonu Kumar <sksonu8544@gmail.com>
2024-05-18 18:42:32 +0530

    dev next js get and post method and metadata

---
+++ src/app/action/get/page.js
@@ -0,0 +1,11 @@
+import GetDataUsingAction from '@/components/GetDataUsingAction'
+import PostDataUsingAction from '@/components/PostDataUsingAction'
+import React from 'react'
+
+export default function page() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-10">
+            <GetDataUsingAction />
+        </main>
+    )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,11 @@
 import GetData1 from "@/components/GetData1";
 import Postdata1 from "@/components/Postdata1";
 
+export const metadata = {
+  title: "Next js Backend",
+  description: "this is next js get and post application..."
+}
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-10">
